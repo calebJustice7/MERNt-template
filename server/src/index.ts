caa7ed795949560a5b61da465ad460316e6fadeb
calebjustice <caleb@ionrev.com>
2023-11-27 20:55:59 -0700

    feat: linting and prettier

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
-  });
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, TypeScript Express !This is a wiered applcationa oidi sdjsdlf sdjsdklj fsd");
+});
 
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-  });
+});
